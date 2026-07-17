e390fcf74176f41ba5ee4dc228018a4e5bfe0329
Fabio Gonçalves <fabiothewarrior@hotmail.com>
2026-07-17 17:18:46 -0300

    Primeiras configurações

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,23 @@
-import "./App.css";
 
-function App() {
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home.tsx";
+import Result from "./pages/Result.tsx";
+import BuyCredit from "./pages/BuyCredit.tsx";
+import Navbar from "./components/Navbar.tsx";
+import Footer from "./components/Footer.tsx";
+
+const App = () => {
 
   return (
-    <>
-      
-    </>
+    <div className="min-h-screen bg-slate-50">
+      <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/result" element={<Result />} />
+          <Route path="/buy" element={<BuyCredit />} />
+        </Routes>
+      <Footer />
+    </div>
   )
 }
 
