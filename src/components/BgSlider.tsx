5d7674e916524f5d91348ae937aed410e5000aa4
Fabio Gonçalves <fabiothewarrior@hotmail.com>
2026-07-22 17:07:06 -0300

    feat: Finalizada a implementação da home e da pagina de resultados

--- src/components/BgSlider.tsx
+++ src/components/BgSlider.tsx
@@ -10,13 +10,17 @@ const BgSlider = () => {
     }
 
     return (
-        <div>
-            <h1 className="text-center text-2xl md:text-3xl lg:text-4xl mt-4 font-semibold bg-linear-to-r from-gray-900 to-gray-400 bg-clip-text text-transparent">
+        <div className="pb-10 sm:py-20 mx-2">
+            <h1 className="mb-12 sm:mb-20 text-center text-2xl md:text-3xl lg:text-4xl mt-4 font-semibold bg-linear-to-r from-gray-900 to-gray-400 bg-clip-text text-transparent">
                 Remova o fundo de suas fotos com <br />alta qualidade e precisão
             </h1>
 
-            <div>
+            <div className="relative w-full max-w-3xl overflow-hidden m-auto rounded-xl">
                 <img src={assets.image_w_bg} style={{clipPath: `inset(0 ${100.2 - sliderPosition}% 0 0)`}} alt="" />
+
+                <img className="absolute top-0 left-0 w-full h-full" src={assets.image_wo_bg} style={{clipPath: `inset(0 0 0 ${sliderPosition}%)`}} alt="" />
+
+                <input className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-full z-10 slider" type="range" min={0} max={100} value={sliderPosition} onChange={sliderChange} name="" id="" />
             </div>
         </div>
     )
--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,8 +1,16 @@
+import { assets } from "../assets/assets";
 
 const Footer = () => {
     return (
-        <footer>
-            footer
+        <footer className="flex items-center justify-between gap-4 px-4 lg:px-44 py-3">
+           <img width={150} src={assets.logo} alt="" />
+           <p className="flex-1 border border-gray-100 pl-4 text-sm text-gray-600 max-sm:hidden">Criado por <a target="blank" href="https://www.linkedin.com/in/fabio-m-gonc/">Fabio Gonçalves</a> | Todos direitos reservados</p>
+
+           <div className="flex gap-1">
+            <img width={40} src={assets.facebook_icon} alt="" />
+            <img width={40} src={assets.twitter_icon} alt="" />
+            <img width={40} src={assets.google_plus_icon} alt="" />
+           </div>
         </footer>
     )
 }
