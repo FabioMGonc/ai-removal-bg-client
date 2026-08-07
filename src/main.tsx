e17e166a8820aed0f9014d027181246a0ed2b77d
Fabio M. Gonçalves <167380233+FabioMGonc@users.noreply.github.com>
2026-08-07 00:12:19 -0300

    Atualizando...

--- src/main.tsx
+++ src/main.tsx
@@ -7,6 +7,8 @@ import { ClerkProvider } from "@clerk/react";
 import AppContextProvider from "./context/AppContextProvider.tsx";
 
 const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
+console.log(import.meta.env);
+console.log(PUBLISHABLE_KEY);
 
 if (!PUBLISHABLE_KEY) {
   throw new Error("Por favor configure a chave");
